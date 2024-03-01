1efa125ed1dfa1ebcdfe845fc43dee4e40612690
Victor Barbosa <vicldb99@gmail.com>
2024-03-01 03:43:14 -0300

    fix: requests

--- src/components/programCredits/index.js
+++ src/components/programCredits/index.js
@@ -3,35 +3,41 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 
 export const ProgramCredits = ({ content, programCreditsData }) => {
   return (
-    <div className="px-8 py-6 flex flex-col gap-2">
-      <h2 className="px-2 font-bold text-xl">{content.creditsTitle}</h2>
-      <div className="px-2 pt-2 flex gap-4 flex-nowrap overflow-x-scroll pb-8">
-        {programCreditsData.map((programCredits, i) => {
-          if (i < 9) {
-            return (
-              <div
-                key={i}
-                className="rounded-lg border-gray-200 bg-white box-border min-w-36 w-36"
-                style={{
-                  borderWidth: "1px",
-                  boxShadow: "0 2px 8px rgba(0,0,0,.1)",
-                }}
-              >
-                <LazyLoadImage
-                  alt={programCredits.name}
-                  className="rounded-t-lg w-full"
-                  src={`https://media.themoviedb.org/t/p/w138_and_h175_face${programCredits?.profile_path}`}
-                />
-                <div className="px-2 py-2">
-                  <div className="font-bold text-sm">{programCredits.name}</div>
-                  <div className="text-xs">{programCredits.character}</div>
-                </div>
-              </div>
-            );
-          }
-          return <></>;
-        })}
-      </div>
-    </div>
+    <>
+      {programCreditsData.length > 0 && (
+        <div className="py-6 flex flex-col gap-2">
+          <h2 className="px-10 font-bold text-xl">{content.creditsTitle}</h2>
+          <div className="px-10 pt-2 flex gap-4 flex-nowrap overflow-x-scroll pb-8">
+            {programCreditsData.map((programCredits, i) => {
+              if (i < 9) {
+                return (
+                  <div
+                    key={i}
+                    className="rounded-lg border-gray-200 bg-white box-border min-w-36 w-36"
+                    style={{
+                      borderWidth: "1px",
+                      boxShadow: "0 2px 8px rgba(0,0,0,.1)",
+                    }}
+                  >
+                    <LazyLoadImage
+                      alt={programCredits.name}
+                      className="rounded-t-lg w-full"
+                      src={`https://media.themoviedb.org/t/p/w138_and_h175_face${programCredits?.profile_path}`}
+                    />
+                    <div className="px-2 py-2">
+                      <div className="font-bold text-sm">
+                        {programCredits.name}
+                      </div>
+                      <div className="text-xs">{programCredits.character}</div>
+                    </div>
+                  </div>
+                );
+              }
+              return null;
+            })}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
--- src/components/programDetails/index.js
+++ src/components/programDetails/index.js
@@ -7,77 +7,87 @@ import { fullYearDateMask, numericDateMask } from "../../utils/dateMask";
 
 export const ProgramDetails = ({ programData }) => {
   return (
-    <div
-      className="bg-no-repeat bg-cover"
-      style={{
-        backgroundImage: `url('https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${programData?.backdrop_path}')`,
-        backgroundPosition: "left calc((50vw - 170px) - 340px) top",
-      }}
-    >
-      <div
-        className="top-16 h-100 w-full"
-        style={{
-          backgroundImage:
-            "linear-gradient(to right, rgba(157.5, 178.5, 199.5, 1) calc((50vw - 170px) - 340px), rgba(157.5, 178.5, 199.5, 0.84) 50%, rgba(157.5, 178.5, 199.5, 0.84) 100%)",
-        }}
-      >
-        <div className="px-10 py-8 flex text-white gap-8 items-center flex-wrap md:flex-nowrap">
-          {programData.poster_path !== null ? (
-            <LazyLoadImage
-              className="rounded-xl"
-              alt={programData.title || programData.name}
-              src={`https://media.themoviedb.org/t/p/w300_and_h450_bestv2${programData?.poster_path}`}
-            />
-          ) : (
-            <div
-              style={{
-                backgroundColor: "#dbdbdb",
-              }}
-              className="h-96 w-64 flex items-center justify-center rounded-lg"
-            >
-              <LazyLoadImage
-                alt={programData.title || programData.name}
-                className="h-40 cursor-pointer"
-                effect="blur"
-                src={
-                  "https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg"
-                }
-              />
-            </div>
-          )}
-
-          <div className="flex flex-col gap-4">
-            <div>
-              <h1 className="text-4xl font-bold flex gap-2 flex-wrap">
-                {programData.title || programData.name}
-                <div className="font-light">
-                  {`(${fullYearDateMask(
-                    programData.release_date || programData.first_air_date
-                  )})`}
+    <>
+      {programData.backdrop_path !== undefined && (
+        <div
+          className="bg-no-repeat bg-cover"
+          style={{
+            backgroundImage: `url('https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${programData?.backdrop_path}')`,
+            backgroundPosition: "left calc((50vw - 170px) - 340px) top",
+          }}
+        >
+          <div
+            className="top-16 h-100 w-full"
+            style={{
+              backgroundImage:
+                "linear-gradient(to right, rgba(157.5, 178.5, 199.5, 1) calc((50vw - 170px) - 340px), rgba(157.5, 178.5, 199.5, 0.84) 50%, rgba(157.5, 178.5, 199.5, 0.84) 100%)",
+            }}
+          >
+            <div className="px-10 py-8 flex text-white gap-8 items-center flex-wrap md:flex-nowrap">
+              {programData.poster_path !== null ? (
+                <LazyLoadImage
+                  className="rounded-xl"
+                  alt={programData.title || programData.name}
+                  src={`https://media.themoviedb.org/t/p/w300_and_h450_bestv2${programData?.poster_path}`}
+                />
+              ) : (
+                <div
+                  style={{
+                    backgroundColor: "#dbdbdb",
+                  }}
+                  className="h-96 w-64 flex items-center justify-center rounded-lg"
+                >
+                  <LazyLoadImage
+                    alt={programData.title || programData.name}
+                    className="h-40 cursor-pointer"
+                    effect="blur"
+                    src={
+                      "https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg"
+                    }
+                  />
                 </div>
-              </h1>
-              <div>
-                {numericDateMask(
-                  programData.release_date || programData.first_air_date
-                )}{" "}
-                {programData?.genres?.map((genre) => genre.name).join(", ")}
-                {programData.runtime && minutesToHours(programData.runtime)}
+              )}
+
+              <div className="flex flex-col gap-4">
+                <div>
+                  <h1 className="text-4xl font-bold flex gap-2 flex-wrap">
+                    {programData.title || programData.name}
+                    <div className="font-light">
+                      {`(${fullYearDateMask(
+                        programData.release_date || programData.first_air_date
+                      )})`}
+                    </div>
+                  </h1>
+                  <div>
+                    {numericDateMask(
+                      programData.release_date || programData.first_air_date
+                    )}
+                    {" - "}
+                    {programData?.genres
+                      ?.map((genre) => genre.name)
+                      .join(", ")}{" "}
+                    {" - "}
+                    {programData.runtime && minutesToHours(programData.runtime)}
+                  </div>
+                </div>
+                <div className="flex w-36 gap-2 items-center">
+                  <Rating vote_average={roundVote(programData.vote_average)} />
+                  <div className="font-bold text-sm">
+                    Avaliação dos usuários
+                  </div>
+                </div>
+                <div className="italic font-light">{programData.tagline}</div>
+                {programData.overview !== "" && (
+                  <>
+                    <div className="text-xl font-bold">Sinopse</div>
+                    <div className="text-base">{programData.overview}</div>
+                  </>
+                )}
               </div>
             </div>
-            <div className="flex w-36 gap-2 items-center">
-              <Rating vote_average={roundVote(programData.vote_average)} />
-              <div className="font-bold text-sm">Avaliação dos usuários</div>
-            </div>
-            <div className="italic font-light">{programData.tagline}</div>
-            {programData.overview !== "" && (
-              <>
-                <div className="text-xl font-bold">Sinopse</div>
-                <div className="text-base">{programData.overview}</div>
-              </>
-            )}
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
